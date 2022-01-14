7ba20745f401132ec2d1b07d749d7b1597bbd9db
luifer Almendrales <almendralesluifer@gmail.com>
2022-01-14 09:31:22 -0500

    update

---
+++ src/components/CharMarket.vue
@@ -0,0 +1,49 @@
+<template>
+  <apexchart
+    type="radialBar"
+    width="350"
+    :options="chartOptions"
+    :series="series"
+  ></apexchart>
+</template>
+<script>
+import VueApexCharts from "vue3-apexcharts";
+export default {
+  name: "CharMarket",
+  components: {
+    apexchart: VueApexCharts,
+  },
+  data() {
+    return {
+      series: [44, 55, 67, 83],
+      chartOptions: {
+        chart: {
+          height: 250,
+          type: "radialBar",
+        },
+        plotOptions: {
+          radialBar: {
+            dataLabels: {
+              name: {
+                fontSize: "22px",
+              },
+              value: {
+                fontSize: "16px",
+              },
+              total: {
+                show: true,
+                label: "Total",
+                formatter: function (w) {
+                  // By default this function returns the average of all series. The below is just an example to show the use of custom formatter function
+                  return 249;
+                },
+              },
+            },
+          },
+        },
+        labels: ["Samsung", "Huawei", "Xiaomi", "Sony"],
+      },
+    };
+  },
+};
+</script>
---
+++ src/components/CharWeeklySales.vue
@@ -0,0 +1,36 @@
+<template>
+  <apexchart
+    width="350"
+    type="bar"
+    :options="chartOptions"
+    :series="series"
+    height="230"
+  ></apexchart>
+</template>
+<script>
+import VueApexCharts from "vue3-apexcharts";
+export default {
+  name: "CharWeeklySales",
+  components: {
+    apexchart: VueApexCharts,
+  },
+  data() {
+    return {
+      chartOptions: {
+        chart: {
+          id: "vuechart-example",
+        },
+        xaxis: {
+          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
+        },
+      },
+      series: [
+        {
+          name: "series-1",
+          data: [30, 40, 35, 50, 49, 60, 70, 91],
+        },
+      ],
+    };
+  },
+};
+</script>
